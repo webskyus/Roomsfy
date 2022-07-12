9eab50fece2dbdf87b33e120ccaa3e8b3d9e6c4a
Orkhan Abbasov <webskyus@gmail.com>
2022-07-12 08:10:39 +0400

    modify __test__  add Home folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World!
+          Welcome
         </h1>
       </main>
     </div>
