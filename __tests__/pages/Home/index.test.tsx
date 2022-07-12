2bd23365a38efcfbe22b15781bd13452352b2d94
Orkhan Abbasov <webskyus@gmail.com>
2022-07-12 07:59:45 +0400

    upd test folders

--- __tests__/pages/Home/index.test.tsx
+++ __tests__/pages/Home/index.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render, screen } from "../utils/test.utils";
-import HomePage from "pages/index";
+import { render, screen } from "../../utils/test.utils";
+import HomePage from "pages";
 
 describe("HomePage", () => {
   it("should render the heading", () => {
