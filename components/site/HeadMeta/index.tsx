b6f0bfb3a3027e15fe250a71c286e00573420757
Orkhan Abbasov <webskyus@gmail.com>
2022-07-15 09:07:53 +0400

    add fonts and base styles

--- components/site/HeadMeta/index.tsx
+++ components/site/HeadMeta/index.tsx
@@ -20,15 +20,14 @@ const HeadMeta = (props: HeadMetaProps) => {
   const {
     description = DESCRIPTION,
     image = logotype,
-    schemaOrg = [{}],
+    schemaOrg = [{}]
   } = props;
 
   return (
     <Head>
       <meta charSet="UTF-8" />
-      <meta
-        name="viewport"
-        content="initial-scale=1.0, width=device-width, user-scalable=false"
+      <meta name="viewport"
+            content="initial-scale=1.0, width=device-width, user-scalable=false"
       />
       <link rel="stylesheet" href={`${FRONT_DOMAIN}${currentPageUrl}`} />
 
@@ -36,115 +35,90 @@ const HeadMeta = (props: HeadMetaProps) => {
       <meta name="description" content={description} />
 
       {/* Fonts */}
-      <link
-        rel="preload"
-        href="/fonts/haffer/Haffer-Light.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
+      <link rel="preload"
+            href="/fonts/inter/Inter-Light.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
       />
-      <link
-        rel="preload"
-        href="/fonts/haffer/Haffer-Medium.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        href="/fonts/haffer/Haffer-Bold.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
+      <link rel="preload"
+            href="/fonts/inter/Inter-Regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
       />
 
       {/* Favicons */}
-      <link
-        rel="shortcut icon"
-        href="/favicon/favicon.ico"
-        type="image/x-icon"
+      <link rel="shortcut icon"
+            href="/favicon/favicon.ico"
+            type="image/x-icon"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/favicon/android-icon-192x192.png"
+      <link rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/favicon/android-icon-192x192.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+      <link rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon/favicon-96x96.png"
+      <link rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/favicon/favicon-96x96.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+      <link rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
       />
 
-      <link
-        rel="apple-touch-icon"
-        sizes="57x57"
-        href="/favicon/apple-icon-57x57.png"
+      <link rel="apple-touch-icon"
+            sizes="57x57"
+            href="/favicon/apple-icon-57x57.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="60x60"
-        href="/favicon/apple-icon-60x60.png"
+      <link rel="apple-touch-icon"
+            sizes="60x60"
+            href="/favicon/apple-icon-60x60.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="72x72"
-        href="/favicon/apple-icon-72x72.png"
+      <link rel="apple-touch-icon"
+            sizes="72x72"
+            href="/favicon/apple-icon-72x72.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="76x76"
-        href="/favicon/apple-icon-76x76.png"
+      <link rel="apple-touch-icon"
+            sizes="76x76"
+            href="/favicon/apple-icon-76x76.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href="/favicon/apple-icon-114x114.png"
+      <link rel="apple-touch-icon"
+            sizes="114x114"
+            href="/favicon/apple-icon-114x114.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="/favicon/apple-icon-120x120.png"
+      <link rel="apple-touch-icon"
+            sizes="120x120"
+            href="/favicon/apple-icon-120x120.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="144x144"
-        href="/favicon/apple-icon-144x144.png"
+      <link rel="apple-touch-icon"
+            sizes="144x144"
+            href="/favicon/apple-icon-144x144.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="/favicon/apple-icon-152x152.png"
+      <link rel="apple-touch-icon"
+            sizes="152x152"
+            href="/favicon/apple-icon-152x152.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-icon-180x180.png"
+      <link rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-icon-180x180.png"
       />
 
       <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta
-        name="msapplication-TileImage"
-        content="/favicon/ms-icon-144x144.png"
+      <meta name="msapplication-TileImage"
+            content="/favicon/ms-icon-144x144.png"
       />
 
       <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta
-        name="apple-mobile-web-app-status-bar-style"
-        content="black-translucent"
+      <meta name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
       />
 
       {/* Schema.org markup for Google+ */}
@@ -174,8 +148,8 @@ const HeadMeta = (props: HeadMetaProps) => {
             "@type": "LocalBusiness",
             name: TITLE,
             image: `${FRONT_DOMAIN}/${logotype}`,
-            url: FRONT_DOMAIN,
-          }),
+            url: FRONT_DOMAIN
+          })
         }}
       />
 
